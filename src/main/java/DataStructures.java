ff49b18e1d42ecf870eb2b4e306223a5d6b029d5
krattz <nthebep@yahoo.com>
2020-04-06 15:57:48 +0200

    Conways Game of Life

---
+++ src/main/java/DataStructures.java
@@ -0,0 +1,50 @@
+public class DataStructures {
+    public static void main(String[] args) {
+        int M = 10, N = 10;
+
+        String [][] square = {
+                {"+","#","#","+","+","+","+","#","#","+"},
+                {"+","+","+","+","+","+","+","+","+","+"},
+                {"+","+","+","#","#","+","+","+","+","+"},
+                {"+","+","+","+","+","+","+","+","+","+"},
+                {"+","+","+","+","+","+","+","+","+","+"},
+                {"+","+","+","+","+","+","+","+","+","+"},
+                {"+","+","+","+","+","#","#","+","+","+"},
+                {"+","+","+","+","+","+","+","+","+","+"},
+                {"+","+","+","+","+","+","+","+","+","+"},
+                {"+","+","+","+","+","+","+","+","+","+"},
+        };
+        System.out.println("Original Generation");
+        for(int i = 0; i<10;i++){
+            for(int j = 0; j<10;j++){
+                System.out.print(square[i][j]);
+            }
+            System.out.println();
+        }
+
+    nextGeneration(square, M, N);
+}
+    // Function to print next generation
+    static void nextGeneration(String square[][], int M, int N) {
+        String[][] future ;
+        System.out.println("Next Generation");
+
+                for (int i = 0; i < M - 1; i++) {
+                    for (int j = 0; j < N - 1; j++) {
+                        if(square[i][j].matches("#")&& square[i][j+1].matches("#")){
+                            square[i+1][j+1]="#";
+                        }
+                        else if ((square[i][j].matches("#")&& square[i][j+1].matches("#"))&& square[i][j+2].matches("#")) {
+                            square[i][j+2].replace("#","+");
+                        }
+                    }
+                }
+                future=square;
+                for(int i =0;i<M;i++){
+                    for (int j = 0; j <M ; j++) {
+                        System.out.print(future[i][j]);
+                    }
+                    System.out.println();
+                }
+            }
+        }
